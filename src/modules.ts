dfb30b01de5a1fd7746d36314d10f693e1e84dbf
Mehmet Ali Külahçı <345m.ali2007@gmail.com>
2022-07-03 15:37:28 +0300

    feat: package.json dependency sortment

--- src/modules.ts
+++ src/modules.ts
@@ -13,6 +13,7 @@ export function installUiFramework(packageDir: string, uiFramework: any) {
 
   else if (uiFrameworkModule?.isDev === false)
     packageJson.dependencies[uiFrameworkModule.package] = '*'
+  sortPackageJson(packageDir)
 }
 
 export function addExtraModules(packageDir: string, selectedModules: any): void {
@@ -41,10 +42,12 @@ export function addExtraModules(packageDir: string, selectedModules: any): void 
       }
     }
   }
+
   fs.writeFileSync(`${packageDir}/package.json`, JSON.stringify(packageJson, null, 2))
+  sortPackageJson(packageDir)
 }
 
-export function installEslint(packageDir: string): void {
+export function installEslint(packageDir: string) {
   // Parse package.json, add eslint dependencies and scripts
   const packageJson = JSON.parse(fs.readFileSync(`${packageDir}/package.json`, 'utf8'))
 
@@ -55,10 +58,33 @@ export function installEslint(packageDir: string): void {
   // Save package.json and copy eslint configuration
   fs.copyFileSync(`${extrasRoot}/.eslintrc`, `${packageDir}/.eslintrc`)
   fs.writeFileSync(`${packageDir}/package.json`, JSON.stringify(packageJson, null, 2))
+  sortPackageJson(packageDir)
 }
 
 export function updateNuxtConfig(packageDir: string, modules: any) {
   modules = modules.map((module: any) => `"${module}"`)
   const replacedNuxtConfig = nuxtConfig.replace('\nmodules:[]', `\nmodules: [${modules.join(', ')}]`)
   fs.writeFileSync(`${packageDir}/nuxt.config.ts`, replacedNuxtConfig)
+}
+
+function sortPackageJson(packageDir: string) {
+  const packageJson = JSON.parse(fs.readFileSync(`${packageDir}/package.json`, 'utf8'))
+
+  packageJson.devDependencies = Object.keys(packageJson.devDependencies).sort().reduce(
+    (obj: any, key) => {
+      obj[key] = packageJson.devDependencies[key]
+      return obj
+    },
+    {},
+  )
+
+  packageJson.dependencies = Object.keys(packageJson.dependencies).sort().reduce(
+    (obj: any, key) => {
+      obj[key] = packageJson.dependencies[key]
+      return obj
+    },
+    {},
+  )
+
+  fs.writeFileSync(`${packageDir}/package.json`, JSON.stringify(packageJson, null, 2))
 }
